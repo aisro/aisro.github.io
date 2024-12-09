a7fe3f8c0f01c87a78476c5ed5d591880d18e8db
aisro <aisro.org.in@gmail.com>
2024-12-09 16:09:31 +0530

    Fix

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 const modal = document.querySelector('#modal');
 const overlay = document.querySelector('#overlay');
-// const openModal = document.querySelector(#open-modal);
-// const closeModal = document.querySelector(#close-modal);
 
 document.addEventListener("DOMContentLoaded", function() {
     modal.classList.add("open");
